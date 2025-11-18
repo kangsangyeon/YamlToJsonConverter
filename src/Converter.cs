afbc4b9d4c97b524baf05e6ae0edcb7c143e74e8
sanyoni <ksyline1997@gmail.com>
2025-11-18 22:18:09 +0900

    Converter 클래스 추가. app 진입점에서 기능 실행.

---
+++ src/Converter.cs
@@ -0,0 +1,33 @@
+﻿namespace YamlToJsonConverter
+{
+    public class Converter
+    {
+        public void ConvertDirectory(string directoryPath, bool recursive = true)
+        {
+            var searchOption = recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            var yamlFiles = Directory.GetFiles(directoryPath, "*.yaml", searchOption)
+                .Concat(Directory.GetFiles(directoryPath, "*.yml", searchOption));
+
+            foreach (var filePath in yamlFiles)
+            {
+                ConvertFileYamlToJson(filePath);
+            }
+        }
+
+        public void ConvertFileYamlToJson(string filePath)
+        {
+            var yaml = File.ReadAllText(filePath);
+            var deserializer = new YamlDotNet.Serialization.DeserializerBuilder().Build();
+            var yamlObject = deserializer.Deserialize(new StringReader(yaml));
+
+            var serializer = new YamlDotNet.Serialization.SerializerBuilder()
+                .JsonCompatible()
+                .Build();
+
+            var json = serializer.Serialize(yamlObject);
+
+            var jsonFilePath = Path.ChangeExtension(filePath, ".json");
+            File.WriteAllText(jsonFilePath, json);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleAppFramework;
+
+ConsoleApp.Run(args, (string dirPath) =>
+{
+    Console.WriteLine($"dirPath: {dirPath}");
+    var converter = new YamlToJsonConverter.Converter();
+    converter.ConvertDirectory(dirPath, true);
+});
